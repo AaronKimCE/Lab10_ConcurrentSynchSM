90abce7efc2eccfa312a0d9fda0a7c3ed69b7e11
Aaron Kim <akim106@wch136-22.cs.ucr.edu>
2019-11-04 12:34:11 -0800

    Working on exercise 1

--- source/main.c
+++ source/main.c
@@ -1,23 +1,60 @@
 /*	Author: akim106
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 10  Exercise 1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+volatile unsigned char TimerFlag = 0;
+
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerOn() {
+    TCCR1B = 0x0B;
+    OCR1A = 125;
+    TIMSK1 = 0x02;
+    TCNT1 = 0;
+    _avr_timer_cntcurr = _avr_timer_M;
+    SREG |= 0x80;
+}
+
+void TimerOff() {
+    TCCR1B = 0x00;
+}
+
+void TimerISR() {
+    TimerFlag = 1;
+}
+
+ISR(TIMER1_COMPA_vect) {
+    _avr_timer_cntcurr--;
+    if (_avr_timer_cntcurr == 0) {
+      TimerISR();
+      _avr_timer_cntcurr = _avr_timer_M;
+    }
+}
+
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    DDRB = 0xFF; PORTB = 0x00;
 
-    /* Insert your solution below */
+    TimerSet(1000);
+    TimerOn();
     while (1) {
-
+    
     }
     return 1;
 }
