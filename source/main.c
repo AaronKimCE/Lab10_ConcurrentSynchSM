5811e90a8db12c296a982fdad698f86ae82e3030
Aaron Kim <akim106@wch136-10.cs.ucr.edu>
2019-11-08 13:03:40 -0800

    working on part 1

--- source/main.c
+++ source/main.c
@@ -48,13 +48,89 @@ void TimerSet(unsigned long M) {
     _avr_timer_cntcurr = _avr_timer_M;
 }
 
+enum ThreeLEDStates{One, Two, Three} ThreeLEDState;
+enum BlinkingLEDStates{Right, Left} BlinkingLEDState;
+enum CombineLEDStates{Do} CombineLEDState;
+unsigned char threeLED = 0;
+unsigned char blinkingLED = 0;
+
+void ThreeLEDTick() {
+    switch(ThreeLEDState) { //Transitions
+      case One:
+        ThreeLEDState = Two;
+        break;
+      case Two:
+        ThreeLEDState = Three;
+        break;
+      case Three:
+        ThreeLEDState = One;
+        break;
+    }
+    switch(ThreeLEDState) { //Actions
+      case One:
+        threeLED = 0x01;
+        break;
+      case Two:
+        threeLED = 0x02;
+        break;
+      case Three:
+        threeLED = 0x04;
+        break;
+    }
+}
+
+void BlinkingLEDTick() {
+    switch(BlinkingLEDState) { //Transitions
+      case Right:
+        BlinkingLEDState = Left;
+        break;
+      case Left:
+        BlinkingLEDState = Right;
+        break;
+    }
+    switch(BlinkingLEDState) { //Actions
+      case Right:
+        blinkingLED = 0x04;
+        break;
+      case Left:
+        blinkingLED = 0x01;
+        break;
+    }
+}
+
+void CombineLEDTick() {
+    switch(CombineLEDState) {
+      case Do:
+        CombineLEDState = Do;
+        break;
+    }
+    switch(CombineLEDState) {
+      case Do:
+        PORTB = threeLED | blinkingLED;
+        break;
+    }
+} 
+
 int main(void) {
     DDRB = 0xFF; PORTB = 0x00;
-
-    TimerSet(1000);
+    unsigned long count1 = 0;
+    unsigned long count2 = 0;
+    TimerSet(1);
     TimerOn();
     while (1) {
-    
+      if (count1 < 1000) {
+        count1++;
+      } else {
+        ThreeLEDTick();
+        count1 = 0;
+      }
+      if (count2 < 1000) {
+        count2++;
+      } else {
+        BlinkingLEDTick();
+        count2 = 0;
+      }
+      CombineLEDTick();
     }
     return 1;
 }
